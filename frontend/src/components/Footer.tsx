8b8cc2c22351872b75852a9542e83106ba6d4e66
Ludvig Möller <118345828+ludvig-moller@users.noreply.github.com>
2025-09-07 23:23:00 +0200

    Add footer

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,11 @@
+
+function Footer() {
+    return (
+        <footer>
+            <span>BroadBoard made by Ludvig Möller</span>
+            <a href="https://github.com/ludvig-moller/broad-board">GitHub Repo</a>
+        </footer>
+    );
+}
+
+export default Footer;
