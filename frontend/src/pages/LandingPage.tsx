5fc42bec702074d9c292260c9c2113a93a6f61cc
Ludvig Möller <118345828+ludvig-moller@users.noreply.github.com>
2025-09-07 23:27:17 +0200

    Add landing page

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -1,7 +1,12 @@
 
 function LandingPage() {
     return (
-        <h1>Landing Page</h1>
+        <>
+            <button>Start a board</button>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore quibusdam, ea dolorem nam fugit dignissimos sed error dolores unde officia placeat illum provident, voluptatum quos accusantium aliquid nisi recusandae praesentium.
+            </p>
+        </>
     );
 }
 
