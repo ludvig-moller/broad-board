4044ec947d286696e02d7de950d67ee30ee8ab96
Ludvig Möller <118345828+ludvig-moller@users.noreply.github.com>
2025-09-23 14:35:22 +0200

    Add models

---
+++ backend/Models/Board.cs
@@ -0,0 +1,11 @@
+﻿using System.Net.WebSockets;
+
+namespace backend.Models
+{
+    public class Board
+    {
+        public required string Id { get; set; }
+        public List<Stroke> Strokes { get; set; } = [];
+        public List<WebSocket> Clients { get; set; } = [];
+    }
+}
---
+++ backend/Models/Stroke.cs
@@ -0,0 +1,15 @@
+﻿namespace backend.Models
+{
+    public class Point { 
+        public float X { get; set; }
+        public float Y { get; set; }
+    }
+    public class Stroke
+    {
+        public required string UserId { get; set; }
+        public required string Id { get; set; }
+        public required string Color { get; set; }
+        public required int LineWidth { get; set; }
+        public List<Point> Points { get; set; } = [];
+    }
+}
