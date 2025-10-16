af678e45b6a61e2c83130135f66d7ac5e67e2cd7
Ludvig Möller <118345828+ludvig-moller@users.noreply.github.com>
2025-10-16 15:46:16 +0200

    add landing page content SCSS style

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -2,12 +2,14 @@ import StartBoardButton from "../components/StartBoardButton";
 
 function LandingPage() {
     return (
-        <>
+        <div className="landing-page-container">
             <StartBoardButton />
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore quibusdam, ea dolorem nam fugit dignissimos sed error dolores unde officia placeat illum provident, voluptatum quos accusantium aliquid nisi recusandae praesentium.
+            <p className="landing-page-info">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                Tempore quibusdam, ea dolorem nam fugit dignissimos sed error dolores unde officia placeat illum provident, 
+                voluptatum quos accusantium aliquid nisi recusandae praesentium.
             </p>
-        </>
+        </div>
     );
 }
 
